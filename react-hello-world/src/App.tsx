897caa92b9864eeb70b1b5f49b7aecfc8eff7f44
Manolya Tamimi <tamimimanolya@gmail.com>
2025-03-23 06:07:16 +0200

    adding browser router, moving app file content to a new component

--- react-hello-world/src/App.tsx
+++ react-hello-world/src/App.tsx
@@ -1,71 +1,15 @@
 import './App.css';
-import Header from './components/header/Header';
-import Categories from './components/categories/Categories';
-import ProductsList from './components/products-list/ProductsList';
-import { useEffect, useReducer, useState } from 'react';
-import WishList from './components/wish-list/WishList';
-import AddProduct from './components/add-product/AddProduct';
-import { EPages } from './enums';
-import { storeReducer } from './reducers/store-reducer';
-import { StoreReducer } from './reducers/store-reducer.types';
-import { storeData } from './utils/storage';
-import CartProvider from './providers/cart-provider';
-
+import Home from './pages/home/home';
+import { BrowserRouter, Routes, Route } from "react-router"
 
 function App() {
-  const [state, dispatch] = useReducer(storeReducer, { isInitialized: false, productList: [], wishList: [] });
-  const [currentPage, setCurrentPage] = useState<EPages>(EPages.CATEGORIES);
-
-  useEffect(() => {
-    if (!state.isInitialized) {
-      dispatch({ type: StoreReducer.EActionTypes.INIT });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (state.isInitialized) {
-      storeData(state.productList, 'products-list');
-    }
-  }, [state.productList, state.isInitialized]);
-
-  useEffect(() => {
-    if (state.isInitialized) {
-      storeData(state.wishList, 'wish-list');
-    }
-  }, [state.wishList, state.isInitialized]);
-
-  const handleAddProduct = (product: Store.IProduct) => {
-    dispatch({ type: StoreReducer.EActionTypes.ADD_PRODUCT, payload: { product } })
-    setCurrentPage(EPages.LIST);
-  }
-
   return (
-    <div>
-      <CartProvider>
-        <Header
-          productsCount={state.productList.length}
-          onNavigate={(page: EPages) => setCurrentPage(page)}
-          currentPage={currentPage}
-        />
-        {currentPage === EPages.CATEGORIES && <Categories />}
-        {currentPage === EPages.WISH && (
-          <WishList
-            wishList={state.wishList}
-            productList={state.productList}
-            onRemove={(id) => dispatch({ type: StoreReducer.EActionTypes.TOGGLE_WISHLIST, payload: { id } })}
-          />
-        )}
-        {currentPage === EPages.ADD && <AddProduct onAdd={handleAddProduct} />}
-        {currentPage === EPages.LIST && (
-          <ProductsList
-            data={state.productList}
-            wishList={state.wishList}
-            onWish={(id) => dispatch({ type: StoreReducer.EActionTypes.TOGGLE_WISHLIST, payload: { id } })}
-            onDelete={(id) => dispatch({ type: StoreReducer.EActionTypes.DELETE_PRODUCT, payload: { id } })}
-          />
-        )}
-      </CartProvider>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<div>login</div>} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ react-hello-world/src/pages/home/home.tsx
@@ -0,0 +1,66 @@
+import { EPages } from '../../enums';
+import { StoreReducer } from '../../reducers/store-reducer.types';
+import { useEffect, useReducer, useState } from 'react';
+import { storeReducer } from '../../reducers/store-reducer';
+import { storeData } from '../../utils/storage';
+import Categories from '../../components/categories/Categories';
+import ProductsList from '../../components/products-list/ProductsList';
+import WishList from '../../components/wish-list/WishList';
+import AddProduct from '../../components/add-product/AddProduct';
+import Header from '../../components/header/Header';
+import CartProvider from '../../providers/cart-provider';
+
+const Home = () => {
+    const [state, dispatch] = useReducer(storeReducer, { isInitialized: false, productList: [], wishList: [] });
+    const [currentPage, setCurrentPage] = useState<EPages>(EPages.CATEGORIES);
+
+    useEffect(() => {
+        if (!state.isInitialized) {
+            dispatch({ type: StoreReducer.EActionTypes.INIT });
+        }
+    }, []);
+
+    useEffect(() => {
+        if (state.isInitialized) {
+            storeData(state.productList, 'products-list');
+        }
+    }, [state.productList, state.isInitialized]);
+
+    useEffect(() => {
+        if (state.isInitialized) {
+            storeData(state.wishList, 'wish-list');
+        }
+    }, [state.wishList, state.isInitialized]);
+
+    const handleAddProduct = (product: Store.IProduct) => {
+        dispatch({ type: StoreReducer.EActionTypes.ADD_PRODUCT, payload: { product } })
+        setCurrentPage(EPages.LIST);
+    }
+    return (
+        <CartProvider>
+            <Header
+                productsCount={state.productList.length}
+                onNavigate={(page: EPages) => setCurrentPage(page)}
+                currentPage={currentPage}
+            />
+            {currentPage === EPages.CATEGORIES && <Categories />}
+            {currentPage === EPages.WISH && (
+                <WishList
+                    wishList={state.wishList}
+                    productList={state.productList}
+                    onRemove={(id) => dispatch({ type: StoreReducer.EActionTypes.TOGGLE_WISHLIST, payload: { id } })}
+                />
+            )}
+            {currentPage === EPages.ADD && <AddProduct onAdd={handleAddProduct} />}
+            {currentPage === EPages.LIST && (
+                <ProductsList
+                    data={state.productList}
+                    wishList={state.wishList}
+                    onWish={(id) => dispatch({ type: StoreReducer.EActionTypes.TOGGLE_WISHLIST, payload: { id } })}
+                    onDelete={(id) => dispatch({ type: StoreReducer.EActionTypes.DELETE_PRODUCT, payload: { id } })}
+                />
+            )}
+        </CartProvider>
+    )
+}
+export default Home;
